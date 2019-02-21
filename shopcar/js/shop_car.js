5a091d7fb2e4ce88cc7687e48fc34941740043eb
wo3330102 <1627239509@qq.com>
2019-02-21 09:50:05 +0800

    '购物车修改六'

--- shopcar/js/shop_car.js
+++ shopcar/js/shop_car.js
@@ -1,65 +1,95 @@
-var shopCar = (function () {
-    var $ul = document.querySelector('ul');
+var shop_car = (function(){
+    var $box = document.querySelector('.main');
     return {
         init() {
             this.event();
-            this.getData();
+            this.insertData(localStorage);
         },
         event() {
             var _this = this;
-            $ul.oninput = function (e) {
-                e = e || window.event;
-                var target =  e.target || e.srcElement;
-                var index = target.parentNode.index;
-                if(target.nodeName === 'INPUT') {
-                    // 修改本地的数据
-                    // 获取当前数据
-                    var data = _this.data[index];
-                    // 修改对应数据的数量
-                    data.count = Number(target.value);
-                    // 更新本地数据
-                    localStorage.shopList = JSON.stringify(_this.data);
-                    // _this.setItem(data);
-                    // 修改小计
+            // 添加单击事件
+            $box.addEventListener('click', function(e){
+                e  = e || window.event;
+                var target = e.target || e.srcElement;
+                if(target.nodeName === 'BUTTON') {
+                    // 获取对应数据的索引
+                    var index = target.parentNode.getAttribute('index')
+                    _this.data.splice(index, 1);
+                    _this.setData();
+                    // 通过数据重新渲染dom
                     _this.insertData(_this.data);
-
-                } 
-            }
-            $ul.onclick = function(e) {
-                e = e || window.event;
-                var target =  e.target || e.srcElement;
-                var pNode = target.parentNode;
-                if(target.nodeName == 'BUTTON') {
-                    _this.data.splice(pNode.index, 1);
-                    console.log(_this.data);
-                    pNode.remove();
-                    localStorage.shopList = JSON.stringify(_this.data);
+                }
+            }, false)
+            $box.onchange = function(e) {
+                e  = e || window.event;
+                var target = e.target || e.srcElement;
+                if(target.nodeName === 'INPUT') {
+                    var index = target.parentNode.getAttribute('index')
+                    // 更新数量
+                    _this.data[index].count = target.value;
+                    _this.setData();
+                    // 通过数据重新渲染dom
+                    _this.insertData(_this.data);
                 }
             }
         },
-        getData() {
-            var shopList = localStorage.shopList || '[]';
-            shopList = JSON.parse(shopList);
-            this.data = shopList;
-            console.log(shopList);
-            this.insertData(shopList)
-
+        // 获取数据
+        // 数据渲染
+        insertData(localStorage) {
+            // var $str = '';
+            var $arr = [];
+            for(var i = 0; i < localStorage.length; i++) {
+                $arr.push(`<table class="cart-merchant-body" id="${localStorage.name}" index=${i}>
+								<tr class="cart-more-buy">
+									<td class="cart-product-buy" colspan="5">
+										<div class="more-buy-tag">
+											<span>加价购</span>
+										</div>
+										<span class="more-buy-tips">另外再加15元起，即可换购超值商品</span>
+										<span class="more-buy-skip">立即加购 ></span>
+									</td>
+								</tr>
+								<tr class="cart-product hasTheBinder">
+									<td class="cart-col-select">
+										<div class="mz-checkbox"></div>
+										<a href="#" class="cart-product-link">
+											<img src="../app/images/icon09.png" class="cart-product-img" />
+										</a>
+										<a href="#" class="cart-product-link cart-product-info">
+											<p class="cart-product-item-name">${localStorage.name}</p>
+											<p class="cart-product-package-name">${localStorage.tc}</p>
+											<p class="cart-product-desc">全网通  ${localStorage.col} ${localStorage.nc}</p>
+										</a>
+									</td>
+									<td class="cart-col-price">
+										<p>
+											<span class="cart-product-price">
+										</span>
+										</p>
+									</td>
+									<td class="cart-col-number" style="text-align: center; margin-left: 20px;">
+										<div class="cart-product-number-adder">
+											<div class="mz-adder">
+												<button class="mz-adder-subtract"></button>
+												<div class="mz-adder-num">
+													<input type="text" class="mz-adder-input" value="1" />
+												</div>
+												<button class="mz-adder-add disabled"></button>
+											</div>
+										</div>
+									</td>
+									<td class="cart-col-total">
+										<span class="cart-product-price total main-goods">
+									</span>
+									</td>
+									<td class="cart-col-ctrl">
+										<div class="cart-product-remove "></div>
+									</td>
+								</tr>
+							</table>`);
+            }
+            $box.innerHTML = $arr.join('');
         },
-        insertData(data) {
-            $ul.innerHTML = '';
-            data.forEach((item, index) => {
-                var $li = document.createElement('li');
-                $li.index = index;
-                $li.innerHTML = `
-                    商品名称:<span class='title'>${data[index].title}</span></br>
-                    商品价格<span class='price'>${data[index].price}</span></br>
-                    购买数量<input class="count" value=${data[index].count} placeholder="请输入数量" /></br>
-                    小计<span>${data[index].price * data[index].count}</span></br>
-                    <button>删除</button>
-                `
-                $ul.appendChild($li);
-            })
-        }
     }
-
 }())
+shop_car.init();
